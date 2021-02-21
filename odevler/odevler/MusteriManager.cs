dd8559c9e0cb38aadcafbab50bf7d3e77083feef
Diyar Yıldırım <Diyary2000@gmail.com>
2021-02-21 15:26:24 +0300

    first commit

---
+++ odevler/odevler/MusteriManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace odevler
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Eklendi : " + " " + musteri.MusteriId + " " + musteri.MusteriName + " " + musteri.MusteriSurname);
+        }
+
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Silindi : " + " " + musteri.MusteriId + " " + musteri.MusteriName + " " + musteri.MusteriSurname);
+        }
+
+        public void Listele(Musteri[] musteri)
+        {
+            foreach (var item in musteri)
+            {
+                Console.WriteLine("Musteri Adı : " + item.MusteriName);
+                Console.WriteLine("Musteri Soyadı : " + item.MusteriSurname);
+
+            }
+
+        }
+
+        internal void Listele(Musteri musteri1) => throw new NotImplementedException();
+    }
+}
---
+++ odevler/odevler/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace odevler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriName = "İbrahim ethem";
+            musteri1.MusteriSurname = "İspir";
+            musteri1.MusteriId = 50468524;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriName = "Eren";
+            musteri2.MusteriSurname = "Kala";
+            musteri2.MusteriId = 25489654;
+
+            MusteriManager musteriEkle = new MusteriManager();
+            musteriEkle.Ekle(musteri1);
+            musteriEkle.Ekle(musteri2);
+
+            MusteriManager musterisil = new MusteriManager();
+            musterisil.Ekle(musteri2);
+
+
+            Musteri[] musteriList = { musteri1, musteri2 };
+            foreach (var musteri in musteriList)
+            {
+                Console.WriteLine(musteri.MusteriName);
+                Console.WriteLine(musteri.MusteriSurname);
+                Console.WriteLine(musteri.MusteriId);
+            }
+
+            MusteriManager musteriListe = new MusteriManager();
+            musteriListe.Listele(musteri1);
+            musteriListe.Listele(musteri2);
+
+            Console.ReadLine();
+        }
+    }
+}
